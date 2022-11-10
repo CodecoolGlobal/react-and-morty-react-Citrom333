92d124bb8fac838dea99ce3606aa590895ea1b53
Citrom333 <citrom333@gmail.com>
2022-11-10 15:06:35 +0100

    test loading pass

--- react-and-morty/src/App.test.js
+++ react-and-morty/src/App.test.js
@@ -3,7 +3,18 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders landing page', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/characters/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByRole, getByText } = render(<App />);
+ 
+  const charButtonElement = getByRole('button', {name: /characters/i});
+  expect(charButtonElement).toBeInTheDocument();
+
+  const locButtonElement = getByRole('button', {name: /locations/i});
+  expect(locButtonElement).toBeInTheDocument();
+
+  const logoElement = getByRole('img', {name: /logo/i});
+  expect(logoElement).toBeInTheDocument();
+
+  const greetingElement = getByText(/multiverse/i);
+  expect(greetingElement).toBeInTheDocument();
+
 });
